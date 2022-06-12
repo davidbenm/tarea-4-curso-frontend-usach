d55ef9e8dcda68bb493f53c31b31fa1df01e7144
davidbenm <99294377+davidbenm@users.noreply.github.com>
2022-06-11 21:10:40 -0400

    Correción a la definición de variables

--- promedioNotas.js
+++ promedioNotas.js
@@ -1,15 +1,15 @@
-notas = [6, 8, 9, 2, 5, 10];
+const notas = [6, 8, 9, 2, 5, 10];
 let suma = 0;
 
 function sumaNotas(conjuntoASumar) {
-    for (i = 0; i < conjuntoASumar.length; i++) {
+    for (let i = 0; i < conjuntoASumar.length; i++) {
         suma = suma + conjuntoASumar[i];    
     }
     return suma
 }
 
 function promedioNotas() {
-    promedio = suma / notas.length;
+    let promedio = suma / notas.length;
     return promedio.toFixed(2) // redondeado el promedio a dos lugares decimales
 }
 
